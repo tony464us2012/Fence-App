12e153ce58a96d50f02116f59aacd3745dcd8703
Anthony <tony464us2011@yahoo.com>
2020-06-25 23:40:11 -0400

    ready

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/login', require('./routes/login_route'));
-app.use('/register', require('./routes/register_route'));
-app.use('/setup', require('./routes/setup_route') );
+app.use('/api/login', require('./routes/login_route'));
+app.use('/api/register', require('./routes/register_route'));
+app.use('/api/setup', require('./routes/setup_route') );
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
