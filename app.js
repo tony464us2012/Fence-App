ed51dfaa32b0053b70803c01650431bef16721f2
Anthony <tony464us2011@yahoo.com>
2020-06-25 23:51:36 -0400

    ready

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/api/setup', require('./routes/setup_route') );
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname + 'client' + 'build' + 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname + 'client', 'build', 'index.html')))
 }
 
 
