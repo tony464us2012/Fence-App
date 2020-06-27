067da1737905c5d0a6a2762d1541392b9ae022de
Anthony <tony464us2011@yahoo.com>
2020-06-27 00:31:56 -0400

    ready

--- client/src/components/pages/UpdateSuccess.js
+++ client/src/components/pages/UpdateSuccess.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const UpdateSuccess = () => {
+const styles= {
+    backgroundColor: 'ivory',
+    padding: '2rem',
+    marginTop: '6rem'
+    };
+const styles2= {
+    height: '100vh'
+};
+
+
+const UpdateSuccess = (props) => {
+    
+    const gotoEstimate = async (e) => {
+    props.history.push('./');
+     };
+    
     return (
-        <div>
-            <h2>Successfully updated</h2>
+        <div className="setupContainer" >
+            <div className="setup_logo"></div>
+            <form id="setupForm" style={styles}>
+            <h2 className="success-title">Successfully updated</h2>
+            <div className="success-btn-container">
+            <a className="btn btn-light border-dark estimate-btn" onClick={ () => gotoEstimate()}>Go To Estimate</a>
+            <a className="btn btn-dark estimate-btn" href="https://myfencekings.com">Home Page</a>
+            </div>
+            </form>
         </div>
     )
 }
